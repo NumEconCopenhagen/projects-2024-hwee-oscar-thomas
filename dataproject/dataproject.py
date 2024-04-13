d366c6b028e8b5fa62c40b97a44b873e8abe750f
mng318 <e0772808@u.nus.edu>
2024-04-13 23:48:20 +0200

    changes

--- dataproject/dataproject.py
+++ dataproject/dataproject.py
@@ -14,3 +14,14 @@ def keep_regs(df, regs):
         df = df.loc[I == False] # keep everything else
     
     return df
+
+def calculate_returns(dataframe):
+    # Calculate monthly returns using the pct_change() function
+    monthly_return = dataframe.pct_change()
+
+    # Calculate cumulative returns using the cumprod() function
+    cumulative_return = (1 + monthly_return).cumprod()
+
+    # Returns monthly and cumulative stock returns
+    return monthly_return, cumulative_return
+
